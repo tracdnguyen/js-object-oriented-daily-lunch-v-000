77789e24917d0704f3e4a3903197fc1f118d4513
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-07 18:59:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+let mealId = 0;
+let customerId = 0;
+let deliveryId = 0;
