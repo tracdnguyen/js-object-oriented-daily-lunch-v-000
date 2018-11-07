bbbdb0a132d0b014d60dcff8c4b87eedd0d8d81a
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-07 19:12:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,5 +65,10 @@ class Meal {
 };
 
 class Delivery {
-
+  constructor(mealId, neighborhoodId, customerId) {
+    this.id = ++deliveryId;
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
+  };
 };
